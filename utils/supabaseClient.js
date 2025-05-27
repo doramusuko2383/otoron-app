aadc6680b0ebe07ffc1783c6c01fd90471f21d3d
doramusuko2383 <doramusuko238@gmail.com>
2025-05-27 19:28:31 +0900

    Update Supabase config

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 import { firebaseAuth } from "../firebase/firebase-init.js";
 
-const supabaseUrl = 'https://xnccwydcesyvqvyqafbg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuY2N3eWRjZXN5dnF2eXFhZmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MDExMTEsImV4cCI6MjA2MjM3NzExMX0.84ELOFGZFJaBNaiHM4roAVmw4o4JMEj4mHnxox1k7Gs';
+const supabaseUrl = 'https://flnqyramgddjcbbaispx.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbnF5cmFtZ2RkamNiYmFpc3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNjEwMDcsImV4cCI6MjA2MzgzNzAwN30.ARtrCplVHw7Q0gdDjsaoHp6__CNulye_IMWIqFmacqc';
 
 // Firebase の ID トークンをヘッダーに付与する fetch ラッパー
 async function authFetch(input, init = {}) {
