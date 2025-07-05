9bceb84127ca2fa64e27608e7442519840e95bf3
doramusuko2383 <doramusuko238@gmail.com>
2025-07-06 02:11:11 +0900

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,16 +1,13 @@
 export default function handler(req, res) {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://playotoron.com/</loc>
-    <lastmod>2025-07-06</lastmod>
-    <priority>1.0</priority>
-  </url>
-</urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://playotoron.com/</loc>
+      <lastmod>2025-07-06</lastmod>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
 
-  res.writeHead(200, {
-    'Content-Type': 'application/xml',
-    'Cache-Control': 'public, max-age=0, s-maxage=600'  // ← 任意でキャッシュ対策
-  });
-  res.end(xml);
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(sitemap);
 }
